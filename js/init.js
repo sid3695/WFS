7d70112770d7d3eabbea99419984ad64e313df9d
sid3695 <sid3695@gmail.com>
2016-03-28 00:36:12 +0530

    small changes

--- js/init.js
+++ js/init.js
@@ -64,14 +64,14 @@
 /*	Make sure that #header-background-image height is
 /* equal to the browser height.
 ------------------------------------------------------ */
-
+  /*
    $('header').css({ 'height': $(window).height() });
    $(window).on('resize', function() {
 
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
    });
-
+  */
 
 /*----------------------------------------------------*/
 /*	Fade In/Out Primary Navigation
